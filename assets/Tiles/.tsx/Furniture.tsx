974ee1f53a94f0d3a43bbe615ccfe11a7c52e464
psu6810110202 <6810110202@psu.ac.th>
2026-02-28 13:25:02 +0700

    Update tilemap

---
+++ assets/Tiles/.tsx/Furniture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Furniture" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="/assets/Tiles/prop/Furniture.png" trans="ff00ff" width="160" height="160"/>
+</tileset>
